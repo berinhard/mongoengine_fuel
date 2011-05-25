f4030232a1622f535674fc4dca42f19a4c9d9483
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-05-24 22:12:32 -0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- encoding:utf-8 -*-
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='mongoengine_fuel',
+    packages = ['mongoengine_fuel'],
+    version='1.0',
+    author='Bernardo Fontes',
+    author_email='falecomigo@bernardofontes.net',
+    description = 'Factory for MongoDB documents created with mongoengine',
+    license = 'http://www.gnu.org/licenses/lgpl.html',
+    keywords='factory documents mongodb mongoengine',
+)
