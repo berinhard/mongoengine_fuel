108ee78d90146ab14c09c90995fa93edf57793d3
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-05-25 21:17:56 -0300

    Remove unecessary documents

--- tests/documents.py
+++ tests/documents.py
@@ -3,20 +3,5 @@ from mongoengine import *
 class IntegerFieldDocument(Document):
     int_field = IntField()
 
-class IntegerFieldDocumentWithMaxValue(Document):
-    int_field = IntField(max_value=5)
-
-class IntegerFieldDocumentWithZeroMaxValue(Document):
-    int_field = IntField(max_value=0)
-
-class IntegerFieldDocumentWithMinValue(Document):
-    int_field = IntField(min_value=5)
-
-class IntegerFieldDocumentWithZeroMinValue(Document):
-    int_field = IntField(min_value=0)
-
-class IntegerFieldDocumentWithMaxAndMinValue(Document):
-    int_field = IntField(min_value=5, max_value=10)
-
 class BooleanFieldDocument(Document):
     bool_field = BooleanField()
