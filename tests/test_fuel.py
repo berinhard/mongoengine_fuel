fe73d65578ea97aa4b76deadcd2dd0f6e13d22b9
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-05-25 21:10:32 -0300

    Add tests to garantee BooleanField and IntField creation

--- tests/test_fuel.py
+++ tests/test_fuel.py
@@ -5,6 +5,18 @@ from mongoengine_fuel import MongoFuel
 
 class DocumentFuelCreation(MongoTestCase):
 
+    def should_work_for_int_field(self):
+        fuel = MongoFuel(IntegerFieldDocument)
+        document = fuel.create()
+
+        self.assertIsInstance(document.int_field, int)
+
+    def should_work_for_boolean_field(self):
+        fuel = MongoFuel(BooleanFieldDocument)
+        document = fuel.create()
+
+        self.assertIn(document.bool_field, [True, False])
+
     def should_not_override_attrs_setted_by_the_user(self):
         fuel = MongoFuel(IntegerFieldDocument)
         document = fuel.create(int_field=3)
