45fe9a0c2a450a5641fcf911d59244af4ee2cd14
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-05-26 11:15:14 -0300

    Refactor MongoFuel's create method

--- mongoengine_fuel/fuel.py
+++ mongoengine_fuel/fuel.py
@@ -2,27 +2,34 @@ from mongoengine import *
 
 from generators import *
 
+#This structure is necessary beacuse mongoengine fields aren't hashable objects
+fields_generators_set = set([
+    (BooleanField, gen_boolean_value),
+    (StringField, gen_str_value),
+    (FloatField, gen_float_value),
+    (DecimalField, gen_decimal_value),
+    (IntField, gen_int_value),
+])
+
 class MongoFuel():
 
     def __init__(self, document):
         self.document = document
         self.fields = document._fields
+        self._fields_generators = fields_generators_set
 
     def create(self, **attrs):
         for field_name, field in self.fields.items():
             if isinstance(field, ObjectIdField) or field_name in attrs:
                 continue
 
-            if isinstance(field, BooleanField):
-                attrs[field_name] = gen_boolean_value(field)
-            elif isinstance(field, StringField):
-                attrs[field_name] = gen_str_value(field)
-            elif isinstance(field, FloatField):
-                attrs[field_name] = gen_float_value(field)
-            elif isinstance(field, DecimalField):
-                attrs[field_name] = gen_decimal_value(field)
-            else:
-                attrs[field_name] = gen_int_value(field)
+            generator = self._get_generator(field)
+            attrs[field_name] = generator(field)
 
         instance = self.document.objects.create(**attrs)
         return instance
+
+    def _get_generator(self, field):
+        for field_class, generator in self._fields_generators:
+            if isinstance(field, field_class):
+                return generator
