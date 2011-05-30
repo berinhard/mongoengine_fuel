4427a9a64fcc82a3e90280f5738da012a6697961
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-05-30 10:11:48 -0300

    Adds the repo url in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version='1.0',
     author='Bernardo Fontes',
     author_email='falecomigo@bernardofontes.net',
+    url='http://github.com/berinhard/mongoengine_fuel',
     description = 'Factory for MongoDB documents created with mongoengine',
     license = 'http://www.gnu.org/licenses/lgpl.html',
     keywords='factory documents mongodb mongoengine',
